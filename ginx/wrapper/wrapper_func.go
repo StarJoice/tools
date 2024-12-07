54470f1dd207455cd4d1f2e61dd213cc769194a0
StarJoice <2159932849@qq.com>
2024-12-07 21:18:22 +0800

    完善ginx

--- ginx/wrapper/wrapper_func.go
+++ ginx/wrapper/wrapper_func.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"github.com/StarJoice/tools/ginx/errs"
 	"github.com/StarJoice/tools/ginx/gctx"
+	"github.com/StarJoice/tools/ginx/session"
 	"github.com/gin-gonic/gin"
 	"log/slog"
 	"net/http"
@@ -75,5 +76,71 @@ func WithRequest[Req any](fn func(ctx *Context, req Req) (Result, error)) gin.Ha
 		}
 		// 成功时返回 200 状态码和结果
 		ctx.JSON(http.StatusOK, res)
+	}
+}
+
+// WithSessionAndRequest 的意思是，传入的业务逻辑方法可以接受 req 和 sess 两个参数
+func WithSessionAndRequest[Req any](fn func(ctx *Context, req Req, sess session.Session) (Result, error)) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		gtx := &Context{Context: ctx}
+		sess, err := session.Get(gtx)
+		if err != nil {
+			slog.Debug("获取 Session 失败", slog.Any("err", err))
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+		var req Req
+		// Bind 方法本身会返回 400 的错误
+		if err := ctx.ShouldBind(&req); err != nil {
+			slog.Debug("绑定参数失败", slog.Any("err", err))
+			return
+		}
+		res, err := fn(gtx, req, sess)
+		if errors.Is(err, errs.ErrNoResponse) {
+			slog.Debug("不需要响应", slog.Any("err", err))
+			return
+		}
+		// 如果里面有权限校验，那么会返回 401 错误（目前来看，主要是登录态校验）
+		if errors.Is(err, errs.ErrUnauthorized) {
+			slog.Debug("未授权", slog.Any("err", err))
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+		if err != nil {
+			slog.Error("执行业务逻辑失败", slog.Any("err", err))
+			ctx.PureJSON(http.StatusInternalServerError, res)
+			return
+		}
+		ctx.PureJSON(http.StatusOK, res)
+	}
+}
+
+// WithSession 的意思是，传入的业务逻辑方法仅可以接受 Session 参数
+func WithSession(fn func(ctx *Context, sess session.Session) (Result, error)) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		gtx := &Context{Context: ctx}
+		sess, err := session.Get(gtx)
+		if err != nil {
+			slog.Debug("获取 Session 失败", slog.Any("err", err))
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+		res, err := fn(gtx, sess)
+		if errors.Is(err, errs.ErrNoResponse) {
+			slog.Debug("不需要响应", slog.Any("err", err))
+			return
+		}
+		// 如果里面有权限校验，那么会返回 401 错误（目前来看，主要是登录态校验）
+		if errors.Is(err, errs.ErrUnauthorized) {
+			slog.Debug("未授权", slog.Any("err", err))
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+		if err != nil {
+			slog.Error("执行业务逻辑失败", slog.Any("err", err))
+			ctx.PureJSON(http.StatusInternalServerError, res)
+			return
+		}
+		ctx.PureJSON(http.StatusOK, res)
 	}
 }
